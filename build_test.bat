358544cc54dbcc0d61c5258bf12e354411c1b043
aloktech <meher.ranjan@gmail.com>
2018-10-29 19:12:23 +0530

    build in JDK 10

--- build_test.bat
+++ build_test.bat
@@ -1,8 +1,8 @@
 cls
-set JAVA_HOME=E:\Tools\Java\jdk-11
-set JRE_HOME=E:\Tools\Java\jdk-11
-rem set JAVA_HOME=E:\Tools\Java\jdk-10.0.2
-rem set JRE_HOME=E:\Tools\Java\jdk-10.0.2
+rem set JAVA_HOME=E:\Tools\Java\jdk-11
+rem set JRE_HOME=E:\Tools\Java\jdk-11
+set JAVA_HOME=E:\Tools\Java\jdk-10.0.2
+set JRE_HOME=E:\Tools\Java\jdk-10.0.2
 set MAVEN_HOME=E:\Tools\apache-maven-3.5.4
 set PATH=%JAVA_HOME%\bin;%JRE_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
 
@@ -15,5 +15,5 @@ java -version
 javac -version
 
 rem mvn clean install -Dmaven.test.skip=true
-mvn -X clean install
+mvn clean install
 rem mvn test
